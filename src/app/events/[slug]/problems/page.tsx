660c7cd31e7b97ed377de9531b37d26d47f2ad89
Nikhilesh002 <nikhileshg02@gmail.com>
2024-11-07 00:50:09 +0530

    problems_page_improved

--- src/app/events/[slug]/problems/page.tsx
+++ src/app/events/[slug]/problems/page.tsx
@@ -1,4 +1,5 @@
 import { getProblemsData } from '@/actions/events';
+import { BarChart, Home } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 
@@ -7,8 +8,16 @@ async function Page({ params:{slug}}: { params: { slug: string } }) {
   const problemsData= await getProblemsData(slug);
 
   return (
-    <div className="mt-28">
-      <div className="pt-1 max-w-[800px] min-w-[500px] px-[3px] pb-[3px] overflow-x-auto mx-auto mb-5 text-center bg-[#E1E1E1] rounded-lg">
+    <div className="">
+      <div className="ms-5 mt-8 flex space-x-8 ">
+        <Link href="/">
+          <Home className="h-12 w-12 hover:bg-gray-200 hover:shadow-md hover:shadow-gray-300 bg-gray-100 rounded-md p-2" />
+        </Link>
+        <Link href={`/events/${slug}/leaderboard`}>
+          <BarChart className="h-12 w-12 hover:bg-gray-200 hover:shadow-md hover:shadow-gray-300 bg-gray-100 rounded-md p-2" />
+        </Link>
+      </div>
+      <div className="mt-20 pt-1 max-w-[800px] min-w-[500px] px-[3px] pb-[3px] overflow-x-auto mx-auto mb-5 text-center bg-[#E1E1E1] rounded-lg">
         <h1 className='text-left ps-2 font-medium'>Problems</h1>
         <table className="w-full table-auto border-collapse border font text-sm border-gray-100">
           <thead className="h-[20px] bg-white">
@@ -28,10 +37,10 @@ async function Page({ params:{slug}}: { params: { slug: string } }) {
               >
                 <td className="w-16 border-r border-[#E1E1E1] text-center">{problem.problemIndex}</td>
                 <td className="pe-2 ps-3 border-r border-[#E1E1E1] text-left">
-                  {problem.content.split('*')[0].slice(2).slice(0, -2)}
+                  <a href={problem.problemLink ?? "#"} target='_blank' >{problem.content.split('*')[0].slice(2).slice(0, -2)}</a>
                 </td>
                 <td className="w-20 border-r border-[#E1E1E1] text-center">
-                  <Link href={`/events/${slug}/editorials/${'A'}`}>
+                  <Link href={`/events/${slug}/editorials/${problem.problemIndex}`}>
                     {problem.problemIndex}
                   </Link>
                 </td>
